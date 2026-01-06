ff7ac746c5dfd9fe2a76baf3197c04e4957836ae
James911015 <32137620+James911015@users.noreply.github.com>
2026-01-05 20:02:30 -0500

    load proyect

---
+++ src/test/java/com/devnexuslabs/opticlingo/OpticlingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devnexuslabs.opticlingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpticlingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
